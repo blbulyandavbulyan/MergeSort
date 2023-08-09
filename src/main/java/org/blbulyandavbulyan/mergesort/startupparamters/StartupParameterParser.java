5c173bc10a63a220cbcae8b8488a46b5f66afce4
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-09 18:42:16 +0400

    fori replaced to foreach

--- src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/StartupParameterParser.java
+++ src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/StartupParameterParser.java
@@ -3,22 +3,20 @@ package org.blbulyandavbulyan.mergesort.startupparamters;
 public class StartupParameterParser {
     public StartupParameters getStartupParameters(String[] args){
         StartupParameters startupParameters = new StartupParameters();
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            switch (arg){
-                case "-a"-> startupParameters.setSortMode(SortMode.ASCENDED);
-                case "-d"-> startupParameters.setSortMode(SortMode.DESCENDED);
-                case "-s"->{
-                    if(startupParameters.isInputDataTypeWasSet())
+        for (String arg : args) {
+            switch (arg) {
+                case "-a" -> startupParameters.setSortMode(SortMode.ASCENDED);
+                case "-d" -> startupParameters.setSortMode(SortMode.DESCENDED);
+                case "-s" -> {
+                    if (startupParameters.isInputDataTypeWasSet())
                         throw new RuntimeException("Входной тип данных уже был установлен ранее!");// TODO: 07.08.2023 заменить это на конкретное исключение
                     startupParameters.setInputDataType(InputDataType.STRING);
                 }
-                case "-i"-> startupParameters.setInputDataType(InputDataType.INTEGER);
+                case "-i" -> startupParameters.setInputDataType(InputDataType.INTEGER);
                 default -> {
-                    if(!startupParameters.hasOutputFileName()){
+                    if (!startupParameters.hasOutputFileName()) {
                         startupParameters.setOutputFileName(arg);
-                    }
-                    else startupParameters.addInputFileName(arg);
+                    } else startupParameters.addInputFileName(arg);
                 }
             }
         }
