a4f99d08e173655463682345230d705009ea1662
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-10 19:55:37 +0400

    added exception classes

---
+++ src/main/java/org/blbulyandavbulyan/mergesort/files/iterator/exceptions/FileReadingException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.mergesort.files.iterator.exceptions;
+
+public class FileReadingException extends RuntimeException{
+    public FileReadingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/org/blbulyandavbulyan/mergesort/files/iterator/exceptions/FileWritingException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.mergesort.files.iterator.exceptions;
+
+public class FileWritingException extends RuntimeException{
+    public FileWritingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
