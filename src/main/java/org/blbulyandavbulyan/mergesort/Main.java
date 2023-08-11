dfb8550f1630c49d64bdd5cf1072a0b167e83ce1
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-11 16:59:18 +0400

    added exception handling in main

--- src/main/java/org/blbulyandavbulyan/mergesort/Main.java
+++ src/main/java/org/blbulyandavbulyan/mergesort/Main.java
@@ -1,17 +1,19 @@
 package org.blbulyandavbulyan.mergesort;
 
 import org.blbulyandavbulyan.mergesort.files.FilesMerger;
+import org.blbulyandavbulyan.mergesort.files.iterator.exceptions.FileReadingException;
 import org.blbulyandavbulyan.mergesort.files.iterator.exceptions.FileWritingException;
 import org.blbulyandavbulyan.mergesort.startupparamters.StartupParameterParser;
 import org.blbulyandavbulyan.mergesort.startupparamters.StartupParameters;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.function.Consumer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         StartupParameters startupParameters = new StartupParameterParser().getStartupParameters(args);
         ConvertorResolver convertorResolver = new ConvertorResolver();
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(startupParameters.outputFileName()));){
@@ -20,9 +22,21 @@ public class Main {
                     bufferedWriter.write(t.toString());
                     bufferedWriter.newLine();
                 } catch (IOException e) {
-                    throw new FileWritingException("Ошибка записи в файл " + startupParameters.outputFileName(), e);
+                    throw new FileWritingException("Ошибка записи выходной файл " + startupParameters.outputFileName(), e);
                 }
             });
+        }
+        catch (FileNotFoundException e){
+            System.err.println("Один из указанных входных файлов не найден!");
+            System.err.println(e.getMessage());
+        }
+        catch (FileWritingException | FileReadingException e){
+            System.err.println(e.getMessage());
+            System.err.println(e.getCause().getMessage());
+        }
+        catch (IOException e){
+            System.err.println("Ошибка ввода вывода");
+            System.err.println(e.getMessage());
         }
     }
 }
