fc8b8ab7198432e7ebe2bffdb8a465ae60b173af
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-12 15:59:55 +0400

    replaced RuntimeException on DuplicateTypeArgumentException in StartupParameterParser

--- src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/StartupParameterParser.java
+++ src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/StartupParameterParser.java
@@ -1,15 +1,19 @@
 package org.blbulyandavbulyan.mergesort.startupparamters;
 
+import org.blbulyandavbulyan.mergesort.startupparamters.exceptions.DuplicateTypeArgumentException;
+
 /**
  * Данный класс предназначен для обработки аргументов командной строки и формирования объекта с параметрами
  */
 public class StartupParameterParser {
     /**
      * Обрабатывает аргументы командной строки
+     *
      * @param args аргументы командной строки
      * @return объект, содержащий параметры в удобном для программы представлении
+     * @throws DuplicateTypeArgumentException если в аргументах встречается указания нескольких типов входных данных или дублирующиеся указания
      */
-    public StartupParameters getStartupParameters(String[] args){
+    public StartupParameters getStartupParameters(String[] args) {
         StartupParametersBuilder parametersBuilder = new StartupParametersBuilder();
         boolean inputDataTypeWasSet = false;
         for (String arg : args) {
@@ -18,13 +22,13 @@ public class StartupParameterParser {
                 case "-d" -> parametersBuilder.setSortMode(SortMode.DESCENDED);
                 case "-s" -> {
                     if (inputDataTypeWasSet)
-                        throw new RuntimeException("Входной тип данных уже был установлен ранее!");// TODO: 07.08.2023 заменить это на конкретное исключение
+                        throw new DuplicateTypeArgumentException("Входной тип данных уже был установлен ранее!");// TODO: 07.08.2023 заменить это на конкретное исключение
                     inputDataTypeWasSet = true;
                     parametersBuilder.setInputDataType(InputDataType.STRING);
                 }
                 case "-i" -> {
                     if (inputDataTypeWasSet)
-                        throw new RuntimeException("Входной тип данных уже был установлен ранее!");// TODO: 07.08.2023 заменить это на конкретное исключение
+                        throw new DuplicateTypeArgumentException("Входной тип данных уже был установлен ранее!");// TODO: 07.08.2023 заменить это на конкретное исключение
                     inputDataTypeWasSet = true;
                     parametersBuilder.setInputDataType(InputDataType.INTEGER);
                 }
