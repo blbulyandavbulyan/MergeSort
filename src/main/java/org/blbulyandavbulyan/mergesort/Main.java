1c45ae3198793926f57a92a53cf4119dc5530b80
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-12 16:19:17 +0400

    added additional catch block for command parsing exceptions

--- src/main/java/org/blbulyandavbulyan/mergesort/Main.java
+++ src/main/java/org/blbulyandavbulyan/mergesort/Main.java
@@ -5,6 +5,7 @@ import org.blbulyandavbulyan.mergesort.files.exceptions.FileOperationException;
 import org.blbulyandavbulyan.mergesort.files.exceptions.FileWritingException;
 import org.blbulyandavbulyan.mergesort.startupparamters.StartupParametersParser;
 import org.blbulyandavbulyan.mergesort.startupparamters.StartupParameters;
+import org.blbulyandavbulyan.mergesort.startupparamters.exceptions.StartupParametersParserException;
 
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -16,7 +17,7 @@ public class Main {
     public static void main(String[] args){
         StartupParameters startupParameters = new StartupParametersParser().getStartupParameters(args);
         ConvertorResolver convertorResolver = new ConvertorResolver();
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(startupParameters.outputFileName()));){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(startupParameters.outputFileName()))){
             new FilesMerger(startupParameters.inputFilesNames()).mergeFiles(convertorResolver.resolve(startupParameters.inputDataType()), startupParameters.sortMode(), (Consumer<Object>) t -> {
                 try {
                     bufferedWriter.write(t.toString());
@@ -33,6 +34,10 @@ public class Main {
         catch (FileOperationException e){
             System.err.println(e.getMessage());
             System.err.println(e.getCause().getMessage());
+        }
+        catch (StartupParametersParserException e){
+            System.err.println("Неверные аргументы командной строки");
+            System.err.println(e.getMessage());
         }
         catch (IOException e){
             System.err.println("Ошибка ввода вывода");
