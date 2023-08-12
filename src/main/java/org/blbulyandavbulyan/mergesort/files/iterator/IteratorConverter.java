5f01301de174368652b9f9e909479cfb2c662798
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-12 16:13:21 +0400

    javadoc added for IteratorConverter

--- src/main/java/org/blbulyandavbulyan/mergesort/files/iterator/IteratorConverter.java
+++ src/main/java/org/blbulyandavbulyan/mergesort/files/iterator/IteratorConverter.java
@@ -3,10 +3,20 @@ package org.blbulyandavbulyan.mergesort.files.iterator;
 import java.util.Iterator;
 import java.util.function.Function;
 
+/**
+ * Данные итератор позволяет преобразовывать данные из оборачиваемого им итератором
+ * @param <T> тип элементов в оригинальном итераторе
+ * @param <R> тип элементов, возвращаемых этим итератором
+ */
 public class IteratorConverter<T, R> implements Iterator<R> {
     private final Iterator<T> original;
     private final Function<T, R> converter;
 
+    /**
+     * Создаёт экземпляр данного итератора
+     * @param original оригинальный итератор, из которого будут браться входные данные
+     * @param converter функция для преобразования данных из оригинального итератора в заданный тип
+     */
     public IteratorConverter(Iterator<T> original, Function<T, R> converter) {
         this.original = original;
         this.converter = converter;
