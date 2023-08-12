5aa6ecd448e37498b70b14980b072c1905bad7fa
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-12 15:58:28 +0400

    added StartupParametersParserException and DuplicateTypeArgumentException

---
+++ src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/exceptions/DuplicateTypeArgumentException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.mergesort.startupparamters.exceptions;
+
+public class DuplicateTypeArgumentException extends StartupParametersParserException{
+    public DuplicateTypeArgumentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/blbulyandavbulyan/mergesort/startupparamters/exceptions/StartupParametersParserException.java
@@ -0,0 +1,9 @@
+package org.blbulyandavbulyan.mergesort.startupparamters.exceptions;
+
+import org.blbulyandavbulyan.mergesort.exceptions.ApplicationException;
+
+public class StartupParametersParserException extends ApplicationException {
+    public StartupParametersParserException(String message) {
+        super(message);
+    }
+}
